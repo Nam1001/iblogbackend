f8ff60b0ca32ee68d2f78935d4a33b6001a768af
Dipanshu <dipanshusrivastava04@gmail.com>
2023-06-28 22:55:31 +0530

    Added Enviroment variable

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express=require('express')
 const cors=require('cors')
+const dotenv=require('dotenv')
 const app=express();
+dotenv.config({path:"./config.env"})
 require('./db')
+const PORT=process.env.PORT
 app.use(cors({}))
 app.use(express.json())
 app.use('/api/blog',require('./routes/blog'))
 
-app.listen(5000,()=>{
+app.listen(PORT,()=>{
     console.log('connected')
 })
